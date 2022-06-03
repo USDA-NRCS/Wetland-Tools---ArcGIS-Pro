df321e56665eac646fece77d9af298f323a66a03
Chris Morse <47194402+CEM-NRCS@users.noreply.github.com>
2022-06-03 13:51:38 -0400

    Update Update_GIS_Server_Layers.py
    Server connection fix

--- NRCS_Wetland_Tools_Pro/SUPPORT/Update_GIS_Server_Layers.py
+++ NRCS_Wetland_Tools_Pro/SUPPORT/Update_GIS_Server_Layers.py
@@ -747,11 +747,13 @@ except:
 
 #### Check GeoPortal Connection
 #nrcsPortal = 'https://gis.sc.egov.usda.gov/portal/'     # GeoPortal Production 10.8.1
+#portalToken = extract_CLU_by_Tract.getPortalTokenInfo(nrcsPortal)
 #if not portalToken:
 #arcpy.AddError("Could not generate Portal token! Please login or switch active portal to GeoPortal 10.8.1! Exiting...")
 #exit()
     
 nrcsPortal = 'https://gis-states.sc.egov.usda.gov/portal/'     # GeoPortal States Production 10.8.1
+portalToken = extract_CLU_by_Tract.getPortalTokenInfo(nrcsPortal)
 if not portalToken:
 arcpy.AddError("Could not generate Portal token! Please login or switch active portal to GeoPortal States 10.8.1! Exiting...")
 exit()
